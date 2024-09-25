87ff40d40e90051725675ace7aa79f021ff4f98b
Ayop14 <betollopis15@gmail.com>
2024-09-25 20:32:16 +0200

    5 feature visualization finished. Added project diary to keep track of stuff I learned.

--- first_part.py
+++ first_part.py
@@ -2,6 +2,7 @@
 import pandas as pd
 from read_data import obtain_dataset
 import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
 
 # Turn weight discrete. What are the different weight values? Lets see with a simple histogram
 def weight_visualization():
@@ -31,6 +32,12 @@ def relation_5_features():
     df = obtain_dataset()
     # Turn discrete the weight
     df['weight full'] = pd.cut(df['weight full'], bins=(0,140,160,1000), labels=['light', 'medium', 'heavy'])
+
+    # Turn max speed to speed ranges. Nulls set to 0
+    df.loc[df['max speed'].isna(), 'max speed'] = 1
+    df['max speed'] = pd.cut(df['max speed'], bins=(0, 100, 105, 110, 115,130), labels=[20,40,60,80,100]).astype(int)
+
+
     # 5 feature plot
     weight_to_color_dict = {
         'light': 'red',
@@ -48,9 +55,11 @@ def relation_5_features():
 
     weight_to_color_func = lambda x: weight_to_color_dict[x]
 
+    fig, ax = plt.subplots()
+
     for key in type_to_shape_dict.keys():
         aux = df[df['bike type'] == key]
-        plt.scatter(
+        ax.scatter(
             aux['consumption'], # x
             aux['acceleration'], # Y
             s=aux['max speed'], # Size
@@ -58,11 +67,44 @@ def relation_5_features():
             c=aux['weight full'].apply(weight_to_color_func) # Color
         )
 
-    plt.title('5 feature visualization')
-    plt.xlabel('Consumption  (l/100km)')
-    plt.ylabel('Acceleration  0-100km (s)')
-    plt.legend()
-    plt.savefig('Images/5 feature visualization')
+
+    ax.set_title('5 feature visualization')
+    ax.set_xlabel('Consumption  (l/100km)')
+    ax.set_ylabel('Acceleration  0-100km (s)')
+
+    legend_elements = [Line2D([], [], color='red', label='Light', markersize=5, lw=8),
+                       Line2D([], [], color='green', label='Medium', markersize=5, lw=8),
+                       Line2D([], [], color='blue', label='Heavy', markersize=5, lw=8)]
+
+    legend_elements2 = [Line2D([], [], color='w', marker='o',  label='Sport', markerfacecolor='grey', markersize=10),
+                       Line2D([], [], color='w', marker='s', label='Custom', markerfacecolor='grey', markersize=10),
+                       Line2D([], [], color='w', marker='p', label='Scrambler', markerfacecolor='grey', markersize=10),
+                        Line2D([], [], color='w', marker='*', label='Scooter', markerfacecolor='grey', markersize=15),
+                        Line2D([], [], color='w', marker='P', label='Trail', markerfacecolor='grey', markersize=10),
+                        Line2D([], [], color='w',marker='^', label='Naked', markerfacecolor='grey', markersize=10)]
+
+    legend_elements3 = [Line2D([], [], color='w', marker='o', label='x <= 100', markerfacecolor='grey', markersize=5),
+                        Line2D([], [], color='w', marker='o', label='100 <= x < 105', markerfacecolor='grey', markersize=6),
+                        Line2D([], [], color='w', marker='o', label='105 <= x < 110', markerfacecolor='grey', markersize=7),
+                        Line2D([], [], color='w', marker='o', label='110 <= x < 115', markerfacecolor='grey', markersize=8),
+                        Line2D([], [], color='w', marker='o', label='115 <= x', markerfacecolor='grey', markersize=9)]
+
+
+    leg1 = fig.legend(handles=legend_elements, title='Weight', fontsize='small', bbox_to_anchor=(0.9,0.875))
+    leg2 = fig.legend(handles=legend_elements2, title='Bike type', loc='upper right', fontsize='small', bbox_to_anchor=(0.9,0.7))
+    leg3 = fig.legend(handles=legend_elements3, title='Max speed (Km/h)', loc='upper left', fontsize='small', bbox_to_anchor=(0.125,0.875))
+
+    ax.add_artist(leg1)
+    ax.add_artist(leg2)
+
+    fig.savefig('Images/5 feature visualization initial')
+
+
+
+from matplotlib.patches import Patch
+
+
+
 
 
 
